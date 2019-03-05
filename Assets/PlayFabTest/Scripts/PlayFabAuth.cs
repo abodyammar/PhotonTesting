b16d397c2b21fe3b2fe9670f193a66c9aecf7f34
Nicole Zhang <nicolezhang94@gmail.com>
2019-03-05 15:24:28 -0800

    added more photon networking and playfab functionality

--- Assets/PlayFabTest/Scripts/PlayFabAuth.cs
+++ Assets/PlayFabTest/Scripts/PlayFabAuth.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
+using System.Collections.Generic;
 
-public class PlayFabAuth : MonoBehaviour {
+//public class PlayFabAuth : MonoBehaviour {
+public class PlayFabAuth : MonoBehaviourPunCallbacks {
 
     private string _playFabPlayerIdCache;
+
+    [SerializeField]
+    private Text roomListUI;
 
     [SerializeField]
     private Text debugLog;
@@ -18,6 +23,9 @@ public class PlayFabAuth : MonoBehaviour {
 
     [SerializeField]
     private InputField passwordInput;
+
+    [SerializeField]
+    private InputField roomName;
 
     [SerializeField]
     private GameObject authPanel;
@@ -35,34 +43,85 @@ public class PlayFabAuth : MonoBehaviour {
     public void OnSignUp() {
         LogMessage("PlayFab signing up new user");
         PlayFabClientAPI.RegisterPlayFabUser(new RegisterPlayFabUserRequest() {
+            TitleId = titleID,
             Email = emailInput.text,
             Password = passwordInput.text,
-            TitleId = titleID,
             RequireBothUsernameAndEmail = false,
 //            DisplayName = nameInput.text
         }, RequestPhotonSignUpToken, OnPlayFabError);
     }
 
-    public void OnLogin() {
-        LogMessage("PlayFab login with email");
+    public void OnEmailLogin() {
+        LogMessage("PlayFab login with email: " + emailInput.text);
         PlayFabClientAPI.LoginWithEmailAddress(new LoginWithEmailAddressRequest() {
+            TitleId = titleID,
             Email = emailInput.text,
-            Password = passwordInput.text,
-            TitleId = titleID
+            Password = passwordInput.text
         }, RequestPhotonLoginToken, OnPlayFabError);
     }
 
+    public void OnAnonymousAndroidLogin() {
+        LogMessage("PlayFab anonymous login with android ID: " + SystemInfo.deviceUniqueIdentifier);
+        PlayFabClientAPI.LoginWithAndroidDeviceID(new LoginWithAndroidDeviceIDRequest() {
+            TitleId = titleID,
+            CreateAccount = true,
+            AndroidDeviceId = SystemInfo.deviceUniqueIdentifier
+        }, RequestPhotonLoginToken, OnPlayFabError);
+    }
+
+    public void OnDisconnect() {
+        LogMessage("Disconnecting from Photon server");
+        PhotonNetwork.Disconnect();
+    }
+
+    public void OnCreateRoomByName() {
+        if (PhotonNetwork.IsConnected) {
+            LogMessage("creating room by name");
+            PhotonNetwork.CreateRoom(roomName.text, new RoomOptions { MaxPlayers = 4 });
+        } else {
+            LogMessage("Not connected to online play");
+//            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public void OnJoinRoomByName() {
+        if (PhotonNetwork.IsConnected) {
+            LogMessage("joining room by name");
+            PhotonNetwork.JoinRoom(roomName.text);
+        } 
+        else {
+            LogMessage("Not connected to online play");
+//            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public void RefreshRooms() {
+        
+    }
+
+    public void OnPlayOnline() {
+        if (!PhotonNetwork.IsConnected) {
+            LogMessage("Connecting...");
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList) {
+        LogMessage("updating room list");
+        foreach (RoomInfo room in roomList) {
+            roomListUI.text += room;
+        }
+    }
+
     private void RequestPhotonSignUpToken(RegisterPlayFabUserResult obj) {
-        LogMessage("successful sign up");
+        LogMessage("Successful sign up");
 
         // auto log in with sign up credentials
-        OnLogin();
+        OnEmailLogin();
     }
 
     private void RequestPhotonLoginToken(LoginResult obj) {
-        LogMessage("successful login");
-
-        LogMessage("PlayFab authenticated. Requesting photon token...");
+        LogMessage("PlayFab authenticated with account: " + obj.PlayFabId + ". Requesting photon token...");
 
         _playFabPlayerIdCache = obj.PlayFabId;
 
@@ -72,7 +131,6 @@ public class PlayFabAuth : MonoBehaviour {
             PhotonApplicationId = PhotonNetwork.PhotonServerSettings.AppSettings.AppIdRealtime
         }, AuthenticateWithPhoton, OnPlayFabError);
     }
-
 
     private void AuthenticateWithPhoton(GetPhotonAuthenticationTokenResult obj) {
         LogMessage("Photon token acquired: " + obj.PhotonCustomAuthenticationToken + "  Authentication complete.");
@@ -114,13 +172,66 @@ public class PlayFabAuth : MonoBehaviour {
         }
     }
 
-    public void OnPlay() {
-        /*
+    public void OnJoinRandom() {
         if (PhotonNetwork.IsConnected) {
+            LogMessage("Joining room...");
             PhotonNetwork.JoinRandomRoom();
         } else {
+            LogMessage("Connecting...");
             PhotonNetwork.ConnectUsingSettings();
-        }*/
+        }
+    }
+
+    /// <summary>
+    /// Called after the connection to the master is established and authenticated
+    /// </summary>
+    public override void OnConnectedToMaster() {
+        // we don't want to do anything if we are not attempting to join a room. 
+        // this case where isConnecting is false is typically when you lost or quit the game, when this level is loaded, OnConnectedToMaster will be called, in that case
+        // we don't want to do anything.
+//        if (PhotonNetwork.IsConnected) {
+            LogMessage("OnConnectedToMaster");
+        
+//        }
+    }
+
+    /// <summary>
+    /// Called when a JoinRandom() call failed. The parameter provides ErrorCode and message.
+    /// </summary>
+    /// <remarks>
+    /// Most likely all rooms are full or no rooms are available. <br/>
+    /// </remarks>
+    public override void OnJoinRandomFailed(short returnCode, string message) {
+        LogMessage("OnJoinRandomFailed. Could not join room.");
+
+//        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 4 });
+    }
+
+
+    /// <summary>
+    /// Called after disconnecting from the Photon server.
+    /// </summary>
+    public override void OnDisconnected(DisconnectCause cause) {
+        LogMessage("OnDisconnected " + cause);
+    }
+
+    public override void OnJoinedRoom() {
+        LogMessage("OnJoinedRoom with " + PhotonNetwork.CurrentRoom.PlayerCount + " Player(s)");
+        Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.\nFrom here on, your game would be running.");
+
+        // #Critical: We only load if we are the first player, else we rely on  PhotonNetwork.AutomaticallySyncScene to sync our instance scene.
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 1) {
+
+        } else {
+
+        }
+    }
+
+    public void TotalPlayers() {
+        LogMessage("current players in room = " + PhotonNetwork.CurrentRoom.PlayerCount);
+        foreach (KeyValuePair<int, Player> player in PhotonNetwork.CurrentRoom.Players) {
+            LogMessage("player " + player.Key + " = " + player.Value);
+        }
     }
 
     /*
@@ -131,42 +242,17 @@ public class PlayFabAuth : MonoBehaviour {
      * We use PlayFabSettings.DeviceUniqueIdentifier as our custom ID.
      * We pass RequestPhotonToken as a callback to be our next step, if 
      * authentication was successful.
-     */
-    /*
-   private void AuthenticateWithPlayFab() {
-       LogMessage("PlayFab authenticating using Custom ID...");
-
-       PlayFabClientAPI.LoginWithCustomID(new LoginWithCustomIDRequest() {
-           CreateAccount = true,
-           CustomId = PlayFabSettings.DeviceUniqueIdentifier
-       }, RequestPhotonToken, OnPlayFabError);
-   }*/
-
-    /*
-    * Step 2
-    * We request Photon authentication token from PlayFab.
-    * This is a crucial step, because Photon uses different authentication tokens
-    * than PlayFab. Thus, you cannot directly use PlayFab SessionTicket and
-    * you need to explicitely request a token. This API call requires you to 
-    * pass Photon App ID. App ID may be hardcoded, but, in this example,
-    * We are accessing it using convenient static field on PhotonNetwork class
-    * We pass in AuthenticateWithPhoton as a callback to be our next step, if 
-    * we have acquired token succesfully
-    */
-    /*
-    private void RequestPhotonToken(LoginResult obj) {
-        LogMessage("PlayFab authenticated. Requesting photon token...");
-
-        //We can player PlayFabId. This will come in handy during next step
-        _playFabPlayerIdCache = obj.PlayFabId;
-
-        PlayFabClientAPI.GetPhotonAuthenticationToken(new GetPhotonAuthenticationTokenRequest() {
-            // NOTE FROM NICOLE: I had to change the line below because .PhotonServerSettings.AppID is incorrect even though that's what it says in the tutorial
-            PhotonApplicationId = PhotonNetwork.PhotonServerSettings.AppSettings.AppIdRealtime
-        }, AuthenticateWithPhoton, OnPlayFabError);
-    }*/
-
-    /*
+     * 
+     * Step 2
+     * We request Photon authentication token from PlayFab.
+     * This is a crucial step, because Photon uses different authentication tokens
+     * than PlayFab. Thus, you cannot directly use PlayFab SessionTicket and
+     * you need to explicitely request a token. This API call requires you to 
+     * pass Photon App ID. App ID may be hardcoded, but, in this example,
+     * We are accessing it using convenient static field on PhotonNetwork class
+     * We pass in AuthenticateWithPhoton as a callback to be our next step, if 
+     * we have acquired token succesfully
+     * 
      * Step 3
      * This is the final and the simplest step. We create new AuthenticationValues instance.
      * This class describes how to authenticate a players inside Photon environment.
